67932c189eb82b544676dc61eee8d8dbb69f0acc
Jean-Marc Prieur <jmprieur@microsoft.com>
2020-11-23 11:56:37 +0100

    Update ConfidentialClientTokenCache/Program.cs
    Co-authored-by: jennyf19 <jeferrie@microsoft.com>

--- ConfidentialClientTokenCache/Program.cs
+++ ConfidentialClientTokenCache/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.Cosmos.Fluent;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.Caching.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Client;
